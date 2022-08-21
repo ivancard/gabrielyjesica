c7d55732fdb043d43fc08047cba2d64da1800318
Ivan Cardenas <ivan.cardenas.35977@hotmail.com>
2022-08-21 11:25:35 -0300

    confim asistance developed

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -3,18 +3,35 @@ export const Confirmation = () => {
   return (
     <section className="confirmation-container">
       <h2>Confirmar asistencia</h2>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="57.692"
+        height="0.5"
+        viewBox="0 0 57.692 0.5"
+      >
+        <path
+          id="Trazado_1"
+          data-name="Trazado 1"
+          d="M-33.846,111.538H23.846"
+          transform="translate(33.846 -111.288)"
+          fill="none"
+          stroke="#707070"
+          strokeWidth="0.5"
+        />
+      </svg>
+
       <div className="form-container">
-        {" "}
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdBqbLEcI6-qEhGVQ_CMmpfMJz2e_Gdo3zghn5yqwnZxREJWA/viewform?embedded=true"
-          width="100%"
-          height="100%"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-        >
-          Cargando…
-        </iframe>
+        <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSdBqbLEcI6-qEhGVQ_CMmpfMJz2e_Gdo3zghn5yqwnZxREJWA/formResponse">
+          <input
+            type="text"
+            name="entry.1995556633"
+            id="name"
+            placeholder="Tu nombre"
+            required
+            autoComplete="off"
+          />
+          <input type="submit" value="Confirmar" />
+        </form>
       </div>
     </section>
   );
