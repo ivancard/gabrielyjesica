0be87c4b757e1fa483a7f984399a00b10415a43a
Ivan Cardenas <ivan.cardenas.35977@hotmail.com>
2022-08-21 15:50:41 -0300

    overlay done

--- src/Invitation.jsx
+++ src/Invitation.jsx
@@ -4,11 +4,13 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Location } from "./components/Location";
 import { Photos } from "./components/Photos";
+import { Overlay } from "./components/Overlay";
 import "./invitation.css";
 
 function Invitation() {
   return (
     <>
+      <Overlay />
       <Header />
       <Location />
       <Photos />
